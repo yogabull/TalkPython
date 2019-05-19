0f5ae7d7c72aa8547c42f2ca65a103729fff7820
yoga <you@example.com>
2019-05-18 23:40:42 -0500

    changed directories which added untracked files

--- guess_number_game/program.py
+++ guess_number_game/program.py
@@ -8,13 +8,17 @@ print()
 the_number = random.randint(0,100)
 
 guess = -1
+name = input('What is your name? ')
 
 while guess != the_number:
     guess = int(input('Guess a number between 0 and 100: ')) #'int' is a class
-
     if guess < the_number:
-        print('The guess is too low.')
+        print('Sorry {1}, your guess of {0} is too low.'.format(guess,name))
     elif guess > the_number:
-        print('The guess is too high.')
+        print('Sorry {}, your guess of {} is too high.'.format(name,guess))
     else:
         print('You win!')
+
+print('All Done {}.'.format(name))
+
+#Using string format converts values into strings. The guess variable is an integer.
