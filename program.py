a0b0e725be595aba1d0a450e3ca1a7af5edfe424
yoga <you@example.com>
2019-05-18 22:58:51 -0500

    cleaning up the folders.

--- program.py
+++ program.py
@@ -1,38 +1,19 @@
-# import random #random module or library to get a random number.
-#
-# print('--------------------------------')
-# print('    GUESS THAT NUMBER GAME')
-# print('--------------------------------')
-# print()
-#
-# the_number = random.randint(0,100)
-#
-# guess = -1
-#
-# while guess != the_number:
-#     guess = int(input('Guess a number between 0 and 100: ')) #'int' is a class
-#     if guess < the_number:
-#         print('The guess is too low.')
-#     elif guess > the_number:
-#         print('The guess is too high.')
-#     else:
-#         print('You win!')
+import random #random module or library to get a random number.
 
-import random
+print('--------------------------------')
+print('    GUESS THAT NUMBER GAME')
+print('--------------------------------')
+print()
 
-print('------------------------------------')
-print('      GUESS THAT NUMBER GAME')
-print('------------------------------------')
-
-number = random.randint(0,100)
+the_number = random.randint(0,100)
 
 guess = -1
 
-while guess != number:
-    guess = int(input('Guess a number between zero and one hundred: '))
-    if guess < number:
-        print('Too low. Guess again.')
-    elif guess > number:
-        print('Too high. Guess again.')
+while guess != the_number:
+    guess = int(input('Guess a number between 0 and 100: ')) #'int' is a class
+    if guess < the_number:
+        print('The guess is too low.')
+    elif guess > the_number:
+        print('The guess is too high.')
     else:
-        print('Correct, you win!')
+        print('You win!')
