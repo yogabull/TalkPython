db277863dfc88d5792308c2a6dae2e136a408c0c
yoga <you@example.com>
2019-05-18 23:44:41 -0500

    I have files outside the repository

---
+++ Birthday App/program.py
@@ -0,0 +1,25 @@
+def print_header():
+    pass
+
+print('--------------------------')
+print('      Birthday App')
+print('--------------------------')
+
+def get_birthday_from_user():
+    pass
+
+
+def compute_days_between_dates():
+    pass
+
+
+def print_birthday_information():
+    pass
+
+
+def main():
+    print_header()
+    bday = get_birthday_from_user()
+    now = None
+    number_of_days = compute_days_between_dates(bday, now)
+    print_birthday_information(number_of_days)
