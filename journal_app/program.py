20d3d5fab86a75e9877e2ebad43e53d51b1a786e
yoga <you@example.com>
2019-05-26 09:50:11 -0500

    first part of journal program exercise. Very cool.

--- journal_app/program.py
+++ journal_app/program.py
@@ -1,12 +1,39 @@
 # This is journal application
 
-def create_journal():
-    pass
+def main():
+    print_header()
+    run_event_loop()
 
 
-def create_entry():
-    pass
+def print_header():
+    print('----------------------------')
+    print('       Journal App')
+    print('----------------------------')
 
 
-def save_entry():
-    pass
+def run_event_loop():
+    print('What do you want to do with your journal?')
+    cmd = None
+
+    while cmd != 'x':
+        cmd = input('List entries [L], Add entry [A], or exit [x]: ')
+        cmd = cmd.lower().strip()
+        if cmd == 'l':
+            list_entries()
+        elif cmd == 'a':
+            add_entries()
+        elif cmd != 'x':
+            print("I don't understand {}.".format(cmd))
+    print('\nGoodbye\n')
+
+
+def list_entries():
+    print('\nListing ...')
+
+
+def add_entries():
+    print('\nAdding ...')
+
+
+
+main()
