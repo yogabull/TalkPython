b415cd18a6eccf2137eb762b5961d34ce7ca0284
yoga <you@example.com>
2019-05-25 22:35:41 -0500

    starting to create local branches to create pull requests on the remote repo.

--- test.py
+++ test.py
@@ -22,21 +22,30 @@ car2.kind = 'Van'
 car2.color = 'Blue'
 car2.value = 100000
 
-c3 = ('Old Brown', 'Hatchback', 'b', 2000)
+c3 = ('Old Brown', 'Hatchback', 'brown', 2000)
 car3 = Vehicle()
 car3.name = c3[0]
 car3.kind = c3[1]
 car3.color = c3[2]
 car3.value = c3[3]
 
+c4 = ('Black Bat', 'hooptie', 'black', 300)
+car4 = Vehicle()
+car4.name = c4[0]
+car4.kind = c4[1]
+car4.color = c4[2]
+car4.value = c4[3]
+
 # test code
 print(car1.description())
 print(car2.description())
 print(car3.description())
+print(car4.description())
 
 print()
 print("{}'s variables were populated with individual strings.".format(car2.name))
 print("{}'s variables were populated from a tuple.".format(c3[0]))
+print("{}'s variables were populated from a tuple and is worth only {}".format(car4.name,str(car4.value)))
 
 print('\nIt might be faster to code class variables from tuples rather than hard-coding individual string variables.\n')
 
