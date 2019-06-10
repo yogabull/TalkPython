65a19989efac81aacb7ed57691b183d7ddc35823
yoga <you@example.com>
2019-06-09 21:42:44 -0500

    Rewrote the birthday app almost without notes. I had to revisit creating a time delta, but I'm feeling pretty good. It's been at least a week since I coded.

--- WKUP/bday.py
+++ WKUP/bday.py
@@ -5,51 +5,43 @@ import datetime
 def header():
     print('---------------------------')
     print('       Birthday App')
-    print('---------------------------')
+    print('---------------------------\n')
 
 
 def get_birthday():
     name = input('What is your name? ')
-    year = int(input('What year were you born [2019]: '))
-    month = int(input('What month were you born [01]: '))
-    day = int(input('What day were you born [01]: '))
+    year = int(input('What year were you born [YYYY]: '))
+    month = int(input('What month were you born [MM]: '))
+    day = int(input('What day were you born [DD]: '))
     print(f'{name}, you were born {month}/{day}/{year}')
-    bday = datetime.date(year,month,day)
-    return bday, name
+    birthday = datetime.date(year,month,day)
+    return birthday, name
 
-def calculate_number_of_days(birthday):
+def calculate_number_of_days(birthday): #returns number of days between bday and today.
     now = datetime.date.today()
-    print(f'The date today is {now}', type(now))
-    number_days = birthday - now
-    number = number_days.days
-    print(f'Number_days [{number_days}] has the class ', type(number_days))
-    print(f'This is converted to the number {number} which now has the class ', type(number))
-    print('Converting the datetime class to and integer lets us do math.\n')
-    # print('the difference is ', number_days, type(number_days))
-    # print(number, type(number))
-    return(number)
+    #NOTE: next line changes birth date year to this year.
+    this_year_bday = datetime.date(now.year, birthday.month, birthday.day)
+    timedelta = this_year_bday - now
+    #NOTE: the next line changes timedelta to a integer from a datetime class.
+    number = timedelta.days
+    return number
 
 
-def message(name, number_days):
-    print(name)
-    number = number_days.days
-    print(number, type(number))
+def message(name, number):
     if number < 0:
         print(f'{name}, your birthday was {-number} days ago.')
     elif number > 0:
         print(f'{name}, your birthday is in {number} days.')
     elif number == 0:
         print(f'Happy Birthday, {name}')
+    print()
 
 
 def main():
     header()
-    # bday = get_birthday()
-    name = 'john'
-    bday = datetime.date(2019, 6, 1)
-    # print(f'The date we are using is {bday}')
+    bday, name = get_birthday()
     number = calculate_number_of_days(bday)
-    # message(name, number)
+    message(name, number)
 
 
 main()
