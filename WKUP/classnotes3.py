fb69cd5258c43874e939f0899b554e567a29e97b
yoga <you@example.com>
2019-06-16 21:47:41 -0500

    commit

--- WKUP/classnotes3.py
+++ WKUP/classnotes3.py
@@ -1,8 +1,12 @@
 #Class notes from Corey Shafer's videos on Youtubeself.
 #Class variables are shared with the classself.
 #Instance variables are unique to an instance.
+# Class variables need tobe applied through the class itself or an instance ofthe class.
 
 class employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
 
     def __init__(self, first, last, pay): #Instance variables for the employee class.
         self.first = first
@@ -10,31 +14,57 @@ class employee:
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
 
+        employee.num_of_emps += 1
+
     def full_name(self):
         return ('{} {}'.format(self.first, self.last))
 
     def apply_raise(self):
-        self.pay = int(self.pay * 1.05)
+        # self.pay = int(self.pay * employee.raise_amount)#option 1 apply class variable.
+        self.pay = int(self.pay * self.raise_amount)#option 2 apply instance variable.
+        #the 2nd version lets you apply a different raise_amount for a single instance.
+        #using self lets sub class override that instance raise_amount.
+
+
+print(employee.num_of_emps, 'number of employees')
 
 emp1 = employee('Gaither', 'McKracken', 70000)
 emp2 = employee('Test', 'User', 50000)
 
-
-
-
-#another example to understand the two variable types: Class and instance variables.
 print(emp1.pay)
-emp1.apply_raise()
+emp1.apply_raise() #this added the attribute to the class.
 print(emp1.pay)
+print()
 
+emp1.raise_amount = 1.06
+print(emp1.__dict__)
 
-class MyClass(object):
-     i = 123
-     def __init__(self):
-         self.i = 345
+print(employee.raise_amount)
+print(emp1.raise_amount)
+print(emp2.raise_amount)
 
-a = MyClass()
-print (a.i, 'This is a instance variable example.')
-# 345
-print (MyClass.i, 'This is a class variable example.')
-# 123
+print(employee.num_of_emps, 'number of employees')
+print('The number of employees changes after the 2 employee classes were instantiated.')
+
+'''
+    print('NOTE:\nemp1.__dict__\nproduces this dictionary for emp1:')
+    print(emp1.__dict__)
+    print()
+    print('NOTE:\nemployee.__dict__\nproduces this information for the employee dictionary:')
+    print(employee.__dict__)
+    print()
+'''
+
+'''
+    #another example to understand the two variable types: Class and instance variables.
+    class MyClass(object):
+         i = 123
+         def __init__(self):
+             self.i = 345
+
+    a = MyClass()
+    print (a.i, 'This is a instance variable example.')
+    # 345
+    print (MyClass.i, 'This is a class variable example.')
+    # 123
+'''
