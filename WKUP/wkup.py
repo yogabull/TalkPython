3087c65f5a0d34a4627ffb8a6ac9fb974f4af61c
yogabull <yogabull@gmail.com>
2021-07-18 12:04:42 -0500

    cleaning up test

--- WKUP/wkup.py
+++ WKUP/wkup.py
@@ -1,4 +1,4 @@
-# make journal app with little help
+# Journal append
 
 def main():
     print_header()
@@ -11,36 +11,32 @@ def print_header():
     print('---------------------------')
     print()
 
+
 def run_event_loop():
     print('What do you want to do with your journal? ')
     cmd = None
-    journal_data = [] # list() means the same thing as the empty brackets.
-
+    journal_data = []
 
     while cmd != 'x':
-        cmd = input('[L] list entries, [A] add entry, or [X] Exit: ')
+        cmd = input('[A] Add entry, [L] List entry, [X] Exit: ')
         cmd = cmd.lower().strip()
         if cmd == 'l':
             list_entries(journal_data)
         elif cmd == 'a':
             add_entry(journal_data)
         elif cmd != 'x':
-            print("Sorry, I don't understand {}.".format(cmd))
+            print(f"Sorry, I don't understand {cmd}. ")
+    print('Thanks, Goodbye.')
 
-    print('Done. Goodbye.')
 
 
 def list_entries(data):
-    print('Your Journal Entries')
-    entries = reversed(data)
-    for entry in enumerate(entries):
-        print(entry)
+    print(data)
 
 
 def add_entry(data):
-    text = input('Type in your entry. <enter> to exit.\n')
-    data.append(text
-
+    text = input('Type your entry. <Enter> to exit. \n')
+    data.append(text)
 
 
 main()
